778342a71fbe4bbd8f19e785f0f88ce7fe9f2a5a
Wink Saville <wink@saville.com>
2025-05-09 12:36:06 -0700

    feat: Break apart the chain
    Try to understand how CadQuery works

--- README.md
+++ README.md
@@ -1,10 +1,55 @@
 # cq-multisection-sweep
 
-Mulitple sections using CadQuery sweep, current 2 a circle(4) and circle(2).
+Mulitple sections using CadQuery sweep with circle(4) and circle(2) along a spline path.
+
+The primary thing I've believed I learned is that there is a "stack" talked about
+in the CadQuery docs and in this example it comes into play when we call the
+consolidateWires is called. But another important notion is list of
+Workplanes that are created when methods such as `pushPoints` and `circle`
+are invoked. This list of Workplanes with a set of objects on in their stacks
+and together a final shape when they when they are in a lazy evaluated when
+"rendering" occurs such as `show` and `export`.
 
 ## Info
 Based on: https://github.com/CadQuery/cadquery/issues/507#issuecomment-729078867
 Some visulization of the docs: https://cadquery.readthedocs.io/en/latest/vis.html
+
+## Usage
+
+Here is the print output of x.py
+```bash
+$ ./x.py 
+wp: <cadquery.cq.Workplane object at 0x7a0ccf051430>
+wp.size(): 0
+type(wp): <class 'cadquery.cq.Workplane'>
+path: <cadquery.cq.Workplane object at 0x7a0be80063f0>
+path.size(): 1
+type(path): <class 'cadquery.cq.Workplane'>
+path.val(): <cadquery.occ_impl.shapes.Edge object at 0x7a0be9a33320>
+type(path.val()): <class 'cadquery.occ_impl.shapes.Edge'>
+first_item: <cadquery.cq.Workplane object at 0x7a0be7ea6d50>, path.size(): 1
+type(first_item): <class 'cadquery.cq.Workplane'>
+loc0: ((50.0, 0.0, -5.0), (90.00000000000001, -8.617315340090216e-14, 178.6728869242176)), path.size(): 1
+loc1: ((48.0, 14.0, -5.0), (90.0, -8.746525374024668e-14, -162.59295659170937)), path.size(): 1
+org_sweep wp.size=1, parents=6, wire_count=0, edge_count=0, obj_count=1
+wp2 wp.size=0, parents=0, wire_count=0, edge_count=0, obj_count=0
+pushPoints(loc0) wp.size=1, parents=1, wire_count=0, edge_count=0, obj_count=1
+circle(2) wp.size=1, parents=2, wire_count=1, edge_count=0, obj_count=1
+pushPoints(loc1) wp.size=1, parents=3, wire_count=1, edge_count=0, obj_count=1
+circle(4) wp.size=1, parents=4, wire_count=2, edge_count=0, obj_count=1
+consolidatedWires wp.size=2, parents=5, wire_count=2, edge_count=0, obj_count=2
+sweep wp.size=1, parents=6, wire_count=0, edge_count=0, obj_count=1
+export sweep wp.size=1, parents=6, wire_count=0, edge_count=0, obj_count=1
+exiting sweep wp.size=1, parents=6, wire_count=0, edge_count=0, obj_count=1
+```
+
+## The images output are identical
+
+Original sweep:
+![image](./org_sweep.png)
+
+Sweep broken down in separate steps:
+![image](./sweep.png)
 
 ## License
 
--- x.py
+++ x.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+#  Based on: https://github.com/CadQuery/cadquery/issues/507#issuecomment-729078867
 import cadquery as cq
 from cadquery.vis import show
 
@@ -8,38 +9,77 @@ y = 0
 z = -5
 
 pts = [(x, y, z), (x, (y+2), z), ((x-2), (y+14), z)]
-path= cq.Workplane("XY").moveTo(x, 0).spline(pts)
-#path= cq.Workplane("XY").spline(pts)
+#path = cq.Workplane("XY").moveTo(x, y).spline(pts) # moveTo does nothing and is not needed
+
+wp = cq.Workplane("XY");
+print(f"wp: {wp}")
+print(f"wp.size(): {wp.size()}")
+print(f"type(wp): {type(wp)}")
+#print(f"dir(wp): {dir(wp)}") # print long list of methods and attributes
+
+path = wp.spline(pts)
+print(f"path: {path}")
+print(f"path.size(): {path.size()}")
+print(f"type(path): {type(path)}")
+#print(f"dir(path): {dir(path)}") # print long list of methods and attributes
+print(f"path.val(): {path.val()}")
+print(f"type(path.val()): {type(path.val())}")
+#print(f"dir(path.val()): {dir(path.val())}") # print long list of methods and attributes
+
+first_item = path.first()
+print(f"first_item: {first_item}, path.size(): {path.size()}")
+print(f"type(first_item): {type(first_item)}")
+#print(f"dir(first_item): {dir(first_item)}") # print long list of methods and attributes
 
 loc0 = [path.val().locationAt(0)]
-print(f"loc0: {loc0[0].toTuple()}")
+print(f"loc0: {loc0[0].toTuple()}, path.size(): {path.size()}")
 loc1 = [path.val().locationAt(1)]
-print(f"loc1: {loc1[0].toTuple()}")
+print(f"loc1: {loc1[0].toTuple()}, path.size(): {path.size()}")
 
-#c2cir = cq.Workplane.pushPoints(loc0).circle(2)
-c2wp = cq.Workplane("XY")
-#show(c2wp, title="c2wp", edges=True, alpha=0.5) # empty
-c2pts = c2wp.pushPoints(loc0)
-#show(c2pts, title="c2pts", edges=True, alpha=0.5) # empty
-c2cir = c2pts.circle(radius)
-show(c2cir, title="c2cir", edges=True, alpha=0.5) # shows just c2cir
+def showCounts(name, wp):
+    wire_count = len(wp.ctx.pendingWires)
+    edge_count = len(wp.ctx.pendingEdges)
+    parent_count = 0
+    wp_pc = wp
+    while(wp_pc.parent):
+        parent_count += 1
+        wp_pc = wp_pc.parent
+    obj_count = len(wp.objects)
+    print(f"{name} wp.size={wp.size()}, parents={parent_count}, wire_count={wire_count}, edge_count={edge_count}, obj_count={obj_count}")
 
-c4cir = c2cir.pushPoints(loc1).circle(radius * 2)
-show(c4cir, title="c4cir-no-consolidateWires", edges=True, alpha=0.5) # shows just c4cir
-#c4cir = c4cir.consolidateWires() # unnecessary, no visible effect on s
-#show(c4cir, title="c4cir-consolidateWires", edges=True, alpha=0.5) # Shows both circles
+# Original code and there are 6 invocations on the Workplane object
+# and showCounts shows the same as showCounts(sweep) below
+org_sweep = (cq.Workplane("XY")
+    .pushPoints(loc0).circle(radius)
+    .pushPoints(loc1).circle(radius * 2)
+    .consolidateWires()
+    .sweep(path,multisection=True)
+)
+showCounts("org_sweep", org_sweep)
+show(org_sweep, title="org_sweep", edges=True, alpha=0.5)
 
-# Create the shape by sweeping the circle along the path
-s = c4cir.sweep(path, multisection=True)
-show(s, title="shape", edges=True, alpha=0.5)
+# The following is the same as the original code but with
+# but we break out into individual steps to show the counts
+wp2 = cq.Workplane("XY")
+showCounts("wp2", wp2)
 
-#cq.exporters.export(shape) # Export as binary STL
-cq.Assembly(s).export("s.stl", exportType="STL", ascii=True) # Export as ASCII STL
+wp2 = wp2.pushPoints(loc0);
+showCounts("pushPoints(loc0)", wp2)
+wp2 = wp2.circle(radius)
+showCounts(f"circle({radius})", wp2)
+wp2 = wp2.pushPoints(loc1);
+showCounts("pushPoints(loc1)", wp2)
+wp2 = wp2.circle(radius * 2)
+showCounts(f"circle({radius * 2})", wp2)
+wp2 = wp2.consolidateWires()
+showCounts(f"consolidatedWires", wp2)
+sweep = wp2.sweep(path,multisection=True)
+showCounts(f"sweep", sweep)
 
-#sweep = (cq.Workplane("XY")
-#    .pushPoints(loc0).circle(2)
-#    .pushPoints(loc1).circle(4)
-#    .consolidateWires()
-#    .sweep(path,multisection=True)
-#    )
-#show(sweep, title="sweep")
+# Export as ASCII STL
+cq.Assembly(sweep).export("sweep.stl", exportType="STL", ascii=True)
+showCounts("export sweep", sweep)
+
+# Show the shape
+show(sweep, title="sweep", edges=True, alpha=0.5)
+showCounts("exiting sweep", sweep)
